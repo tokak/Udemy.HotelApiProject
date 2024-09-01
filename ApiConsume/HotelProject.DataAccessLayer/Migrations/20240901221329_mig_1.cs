71f6e05aedb85af2833b571417ef7ffa81000f4b
Murat Tokak <tokakmurat01@gmail.com>
2024-09-02 01:38:57 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.DataAccessLayer/Migrations/20240901221329_mig_1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelProject.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ServicesID",
+                table: "Services",
+                newName: "ServiceID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ServiceID",
+                table: "Services",
+                newName: "ServicesID");
+        }
+    }
+}
