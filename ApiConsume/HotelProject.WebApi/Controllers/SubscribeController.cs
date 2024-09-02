85a1c84cd65ae103e6f149d79e45d3a37ae6ac10
Murat Tokak <tokakmurat01@gmail.com>
2024-09-02 12:26:12 +0300

    Api Kodlaması, Tema yüklenmesi ve Partialların ayarlanması

---
+++ ApiConsume/HotelProject.WebApi/Controllers/SubscribeController.cs
@@ -0,0 +1,52 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscribeController : ControllerBase
+    {
+        private readonly ISubscribeService _subscribeService;
+
+        public SubscribeController(ISubscribeService subscribeService)
+        {
+            _subscribeService = subscribeService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var values = _subscribeService.TGetList();
+            return Ok(values);
+        }
+        [HttpDelete]
+        public IActionResult DeleteSubscribe(int id)
+        {
+            var values = _subscribeService.TGetByID(id);
+            _subscribeService.TDelete(values);
+            return Ok();
+
+        }
+        [HttpPost]
+        public IActionResult AddSubscribe(Subscribe subscribe)
+        {
+            _subscribeService.TInsert(subscribe);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateSubscribe(Subscribe subscribe)
+        {
+            _subscribeService.TUpdate(subscribe);
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetSubscribe(int id)
+        {
+            var value = _subscribeService.TGetByID(id);
+            return Ok(value);
+        }
+    }
+}
