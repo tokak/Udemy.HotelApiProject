4c1adb9743c909840ee0389bdc0be2637ffb2579
Murat Tokak <tokakmurat01@gmail.com>
2024-10-09 10:13:37 +0300

    iletişim paneli oluşturma

---
+++ ApiConsume/HotelProject.WebApi/Controllers/ContactController.cs
@@ -0,0 +1,50 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        [HttpGet]
+        public IActionResult AboutList()
+        {
+            var values = _contactService.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddAbout(Contact contact)
+        {
+            _contactService.TInsert(contact);
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteAbout(int id)
+        {
+            var values = _contactService.TGetByID(id);
+            _contactService.TDelete(values);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateAbout(Contact contact)
+        {
+            _contactService.TUpdate(contact);
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetContact(int id)
+        {
+            var values = _contactService.TGetByID(id);
+            return Ok(values);
+        }
+    }
+}
