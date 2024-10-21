1481c099b5706c291475e7833ba6d523b5af314c
Murat Tokak <tokakmurat01@gmail.com>
2024-10-21 18:52:00 +0300

    Authorize işlemlerin tamamlanması

--- Frontend/HotelProject.WebUI/Program.cs
+++ Frontend/HotelProject.WebUI/Program.cs
@@ -5,6 +5,8 @@ using FluentValidation;
 using HotelProject.WebUI.Dtos.GuestDto;
 using HotelProject.WebUI.ValidationRules.GuestValidationRules;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,8 +18,21 @@ builder.Services.AddHttpClient();
 builder.Services.AddTransient<IValidator<CreateGuestDto>, CreateGuestValidator>();
 builder.Services.AddTransient<IValidator<UpdateGuestDto>, UpdateGuestValidator>();
 builder.Services.AddControllersWithViews().AddFluentValidation();
-
 builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddMvc(config =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+    .RequireAuthenticatedUser()
+    .Build();
+    config.Filters.Add(new AuthorizeFilter(policy));
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+    options.LoginPath = "/Login/Index/";
+});
 
 var app = builder.Build();
 
@@ -26,10 +41,11 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+app.UseStatusCodePagesWithReExecute("/ErrorPage/Error404","?code{0}");
+app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseAuthentication();    
 app.UseAuthorization();
 
 app.MapControllerRoute(
