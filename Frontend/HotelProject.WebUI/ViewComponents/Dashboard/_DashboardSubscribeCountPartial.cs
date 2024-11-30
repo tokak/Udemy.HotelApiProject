483c865126d4300c853d99292f520b209f379db2
Murat Tokak <tokakmurat01@gmail.com>
2024-11-30 15:51:05 +0300

    sosyal medya takipci sayısı çekme

--- Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using HotelProject.WebUI.Dtos.FollowersDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 namespace HotelProject.WebUI.ViewComponents.Dashboard
 {
     public class _DashboardSubscribeCountPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://instagram-profile1.p.rapidapi.com/getprofile/murattycedag"),
+                Headers =
+    {
+        { "x-rapidapi-key", "39fce84202msh736411f31a4ca64p16c608jsnc5b12e3cde0f" },
+        { "x-rapidapi-host", "instagram-profile1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                ResultInstagramFollowersDto resultInstagramFollowersDto = JsonConvert.DeserializeObject<ResultInstagramFollowersDto>(body);
+                return View(resultInstagramFollowersDto);
+            }
         }
     }
 }
